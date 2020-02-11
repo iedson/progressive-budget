e034bb7af9c4ee67e98398452a03251e30148582
Izzy Edson <isabelle.edson@hyatt.com>
2020-02-11 14:03:10 -0600

    redoing initial state

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,35 @@
-//requires express
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
 
-//will need a get route
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-//will need a post route
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
