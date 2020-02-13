6477725c238e4652885fb9bdf5d83f69a74b56cb
Izzy Edson <isabelle.edson@hyatt.com>
2020-02-13 16:52:41 -0600

    fixing routes & connecting to mongo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,12 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://webuser:Ajn5CSz4n!kx8ju@ds339968.mlab.com:39968/heroku_3498zb24", {
+mongoose.connect(
+  process.env.MONGODB_URI || 
+  "mongodb://webuser:Ajn5CSz4n!kx8ju@ds339968.mlab.com:39968/heroku_3498zb24", 
+{
   useNewUrlParser: true,
   useFindAndModify: false
-});
+}
+);
 
 // routes here
+
+app.use(require('./routes/api'))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
